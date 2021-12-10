a1b9142b1f29d3d6f3bfef4180401225d908df4f
Scott A. Hyslop <sahyslop@gmail.com>
2021-12-10 06:07:48 -0500

    ui components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DeviceListComponent } from './components/device-list/device-list.component';
+import { DeviceComponent } from './components/device/device.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DeviceListComponent,
+    DeviceComponent,
+    SearchComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
